0f2154b96e4f6ce3d13aec0fdbd8e4b12eb72354
janjunt <janjunt@163.com>
2017-08-16 21:45:36 +0800

    LoggerRepository根据组件类型动态创建并监视配置文件 Signed-off-by: janjunt <janjunt@163.com>

--- src/TFramework.Core/Logging/Log4netFactory.cs
+++ src/TFramework.Core/Logging/Log4netFactory.cs
@@ -4,27 +4,25 @@ using System;
 using log4net.Repository;
 using Microsoft.Extensions.Configuration;
 using TFramework.Core.Environment;
+using System.Reflection;
+using log4net;
+using System.Collections.Concurrent;
 
 namespace TFramework.Core.Logging
 {
     public class Log4netFactory : AbstractLoggerFactory
     {
-        private static bool _isFileWatched = false;
-
-        private ILoggerRepository _loggerRepository;
+        private static ConcurrentDictionary<string, ILoggerRepository> _repositoryCache = new ConcurrentDictionary<string, ILoggerRepository>();
+        private static ConcurrentDictionary<string, Assembly> _assemblyCache = new ConcurrentDictionary<string, Assembly>();
+        private readonly string _configFilename;
+        private readonly IHostEnvironment _hostEnvironment;
 
         public Log4netFactory(
-            ILoggerRepository loggerRepository, 
             IConfiguration configuration,
             IHostEnvironment hostEnvironment)
         {
-            _loggerRepository = loggerRepository;
-            var configFilename = configuration["log4net.Config"];
-            if (!_isFileWatched && !string.IsNullOrWhiteSpace(configFilename) && hostEnvironment.IsFullTrust)
-            {
-                XmlConfigurator.ConfigureAndWatch(loggerRepository, GetConfigFile(configFilename));
-                _isFileWatched = true;
-            }
+            _configFilename = configuration["log4net.Config"];
+            _hostEnvironment = hostEnvironment;
         }
 
         public override Castle.Core.Logging.ILogger Create(string name, LoggerLevel level)
@@ -34,7 +32,19 @@ namespace TFramework.Core.Logging
 
         public override Castle.Core.Logging.ILogger Create(string name)
         {
-            return new Log4netLogger(_loggerRepository.GetLogger(name), this);
+            var componentAssembly = _assemblyCache.GetOrAdd(name, n => Type.GetType(n).GetTypeInfo().Assembly);
+            var repository = LogManager.GetRepository(componentAssembly);
+
+            if (!string.IsNullOrWhiteSpace(_configFilename) && _hostEnvironment.IsFullTrust)
+            {
+                _repositoryCache.GetOrAdd(repository.Name, n =>
+                {
+                    XmlConfigurator.ConfigureAndWatch(repository, GetConfigFile(_configFilename));
+                    return repository;
+                });
+            }
+
+            return new Log4netLogger(repository.GetLogger(name), this);
         }
     }
 }
